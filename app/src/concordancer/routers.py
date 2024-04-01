f5db93dc89c961d2d1ed47ae6f76303055243ead
phattradaa <phattrada.mik@dome.tu.ac.th>
2024-04-02 01:34:08 +0700

    build:concordancer
    2/04

--- app/src/concordancer/routers.py
+++ app/src/concordancer/routers.py
@@ -3,3 +3,16 @@ from fastapi import APIRouter
 router = APIRouter(
     tags=["Concordancer"]
 )
+
+# ส่งค่าให้
+# {
+#  "pointFocus" : "คำที่ต้องการsearch",
+# }
+# ต้องการรับค่า
+# {
+# "Data" : [[file_name , leftcontext, rightcontext]
+# ]
+# "information": [จำนวนทั้งหมดที่ได้จากการsearch, peromillion, percent]
+# len ที่สามารถรองรับได้ = 110 ( ตัวอักษร+space)
+# }
+
