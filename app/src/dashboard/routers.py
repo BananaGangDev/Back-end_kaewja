627c398d2866f06244fd407a26e80e1c03643604
phattradaa <phattrada.mik@dome.tu.ac.th>
2024-05-03 02:08:48 +0700

    improve:dashboard

--- app/src/dashboard/routers.py
+++ app/src/dashboard/routers.py
@@ -63,183 +63,188 @@ def create_stat(string,tagset_id,filename,db:db_dependency):
             return Response(content="No tag in this file",status_code=status.HTTP_204_NO_CONTENT)
 
 #Get Summary Stat        
-@router.get("/get_stat",status_code=200)
-def get_stat(tagset_id,db:db_dependency):
-    is_successful,file = global_st.get_all_path_files(in_corpus=False)
-    total_document = len(file)
-    checked_document = dashboard_crud.get_stat_by_id(db=db,tagset_id=tagset_id)
-    roots = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_level=0,label_parent="ROOT")
-    print(checked_document)
-    print(roots)
-    if (checked_document in [[],False,None]) or (roots in [[],None,False]):
-        return Response(content="No data",status_code=status.HTTP_204_NO_CONTENT)
+# @router.get("/get_stat",status_code=200)
+# def get_stat(tagset_id,db:db_dependency):
+#     is_successful,file = global_st.get_all_path_files(in_corpus=False)
+#     total_document = len(file)
+#     checked_document = dashboard_crud.get_stat_by_id(db=db,tagset_id=tagset_id)
+#     roots = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_level=0,label_parent="ROOT")
+#     print(checked_document)
+#     print(roots)
+#     if (checked_document in [[],False,None]) or (roots in [[],None,False]):
+#         return Response(content="No data",status_code=status.HTTP_204_NO_CONTENT)
     
-    path = []
-    for root in roots:
-        string_root = root.label_name
-        all_level1 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=root.label_name,label_level=1)
-        if all_level1:
-            for level1 in all_level1:
-                string_level1 = string_root + "/" + level1.label_name
-                all_level2 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=level1.label_name,label_level=2)
-                if all_level2 :
-                    for level2 in all_level2:
-                        string_level2 = string_level1 + "/" + level2.label_name
-                        all_level3 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=level2.label_name,label_level=3)
-                        if all_level3 :
-                            for level3 in all_level3:
-                                string_level3 = string_level2 + "/" + level3.label_name
-                                path.append([string_level3,0])
-                        else:
-                            path.append([string_level2,0])
-                else:
-                    path.append([string_level1,0])
-        else:
-            path.append([string_root,0])
+#     path = []
+#     for root in roots:
+#         string_root = root.label_name
+#         all_level1 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=root.label_name,label_level=1)
+#         if all_level1:
+#             for level1 in all_level1:
+#                 string_level1 = string_root + "/" + level1.label_name
+#                 all_level2 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=level1.label_name,label_level=2)
+#                 if all_level2 :
+#                     for level2 in all_level2:
+#                         string_level2 = string_level1 + "/" + level2.label_name
+#                         all_level3 = dashboard_crud.get_label_by_root(db=db,tagset_id=tagset_id,label_parent=level2.label_name,label_level=3)
+#                         if all_level3 :
+#                             for level3 in all_level3:
+#                                 string_level3 = string_level2 + "/" + level3.label_name
+#                                 path.append([string_level3,0])
+#                         else:
+#                             path.append([string_level2,0])
+#                 else:
+#                     path.append([string_level1,0])
+#         else:
+#             path.append([string_root,0])
                             
-    df_labels = pd.DataFrame(path,columns=["PATH","Count"])
-    for stat in checked_document:
-        label = dashboard_crud.get_label_by_label_id(db=db,label_id=stat.label_id)[0]
-        label_name = label.label_name
-        count = stat.count
-        # print(label_name)
-        for path in df_labels['PATH']:
-            full_path = path
-            if '/' in path:
-                path = path.split('/')[-1]
-                # print(label_name)
-                # print(path)
+#     df_labels = pd.DataFrame(path,columns=["PATH","Count"])
+#     for stat in checked_document:
+#         label = dashboard_crud.get_label_by_label_id(db=db,label_id=stat.label_id)[0]
+#         label_name = label.label_name
+#         count = stat.count
+#         # print(label_name)
+#         for path in df_labels['PATH']:
+#             full_path = path
+#             if '/' in path:
+#                 path = path.split('/')[-1]
+#                 # print(label_name)
+#                 # print(path)
             
-            if label_name == path:
-                new_value = df_labels['Count'].loc[(df_labels['PATH']==full_path)] + count
-                # print(df_labels.loc[(df_labels['PATH']==full_path)]['Count'])
-                df_labels['Count'].loc[(df_labels['PATH']==full_path)] = new_value
+#             if label_name == path:
+#                 new_value = df_labels['Count'].loc[(df_labels['PATH']==full_path)] + count
+#                 # print(df_labels.loc[(df_labels['PATH']==full_path)]['Count'])
+#                 df_labels['Count'].loc[(df_labels['PATH']==full_path)] = new_value
     
-    raw_df = df_labels.loc[df_labels['Count'] > 0]
-    # print(raw_df)
-    root_names = []
-    all_count = 0
-    for index in raw_df.index.values.tolist() :
-        # print(df)
-        #print(raw_df['PATH'].loc[(raw_df.index==index)])
-        tmp = raw_df['PATH']
-        root = tmp[index].split('/')[0]
-        if root not in root_names :
-            root_names.append(root)
-        all_count+=raw_df['Count'][index]
+#     raw_df = df_labels.loc[df_labels['Count'] > 0]
+#     # print(raw_df)
+#     root_names = []
+#     all_count = 0
+#     for index in raw_df.index.values.tolist() :
+#         # print(df)
+#         #print(raw_df['PATH'].loc[(raw_df.index==index)])
+#         tmp = raw_df['PATH']
+#         root = tmp[index].split('/')[0]
+#         if root not in root_names :
+#             root_names.append(root)
+#         all_count+=raw_df['Count'][index]
     
-    data = []
+#     data = []
     
-    for root_name in root_names:
-        child_data = [] 
-        for index in raw_df.index.values.tolist() :
-            tmp = raw_df['PATH']
-            root_tmp = tmp[index].split('/')[0]
-            child_name = tmp[index].split('/')[-1]
-            if root_tmp == root_name:
-                # child = dashboard_crud.get_label_by_label_name(db=db,label_name=child_name)
-                # print(raw_df['Count'][index],type(raw_df['Count'][index]))
-                # percent = (raw_df['Count'][index].item()/all_count)*100
-                count = raw_df['Count'][index].item()
-                json = {
-                    "child_name" : child_name,
-                    "child_description" : dashboard_crud.get_label_description(db=db,label_name=child_name),
-                    "count" : count,
-                    "percent" : str((count/all_count)*100)
-                }
-                child_data.append(json)
+#     for root_name in root_names:
+#         child_data = [] 
+#         for index in raw_df.index.values.tolist() :
+#             tmp = raw_df['PATH']
+#             root_tmp = tmp[index].split('/')[0]
+#             child_name = tmp[index].split('/')[-1]
+#             if root_tmp == root_name:
+#                 # child = dashboard_crud.get_label_by_label_name(db=db,label_name=child_name)
+#                 # print(raw_df['Count'][index],type(raw_df['Count'][index]))
+#                 # percent = (raw_df['Count'][index].item()/all_count)*100
+#                 count = raw_df['Count'][index].item()
+#                 json = {
+#                     "child_name" : child_name,
+#                     "child_description" : dashboard_crud.get_label_description(db=db,label_name=child_name),
+#                     "count" : count,
+#                     "percent" : str((count/all_count)*100)
+#                 }
+#                 child_data.append(json)
         
-        data.append({
-            "root_name" : root_name,
-            "data" : child_data
-        })
+#         data.append({
+#             "root_name" : root_name,
+#             "data" : child_data
+#         })
     
-    card_data = {
-        "total" : str(total_document),
-        "check" : str(len(checked_document)),
-        "error" : str(all_count)
-    }
+#     card_data = {
+#         "total" : str(total_document),
+#         "check" : str(len(checked_document)),
+#         "error" : str(all_count)
+#     }
     
-    output = {"card_data":card_data,
-            "data":data
-    }
-    return output
+#     output = {"card_data":card_data,
+#             "data":data
+#     }
+#     return output
 
 #Get Stat Card In DB
 @router.get("/get_stat_from_db",status_code=200)
 def get_stat_by_id(tagset_id,db:db_dependency):
     return dashboard_crud.get_stat_by_id(db=db,tagset_id=tagset_id)
 
-# @router.get("/get_stat_version2",status_code=200)
-# def get_stat_version2(tagset_id,db:db_dependency):
-#     is_successful,file = global_st.get_all_path_files(in_corpus=False)
-#     total_document = len(file)
-#     checked_document = dashboard_crud.get_stat_by_id(db=db,tagset_id=tagset_id)
-#     if (checked_document in [[],False,None]) or (not is_successful):
-#         return Response(content="No data",status_code=status.HTTP_204_NO_CONTENT)
+@router.get("/get_stat",status_code=200)
+def get_stat(tagset_id,db:db_dependency):
+    is_successful,file = global_st.get_all_path_files(in_corpus=False)
+    total_document = len(file)
+    checked_document = dashboard_crud.get_stat_by_id(db=db,tagset_id=tagset_id)
+    if (checked_document in [[],False,None]) or (not is_successful):
+        return Response(content="No data",status_code=status.HTTP_204_NO_CONTENT)
     
-#     total_error = 0
-#     data = []
-#     for i in checked_document:
-#         total_error += int(i.count)
+    total_error = 0
+    data = []
+    for i in checked_document:
+        total_error += int(i.count)
         
-#     for stat in checked_document:
-#         my_label = dashboard_crud.get_label_by_label_id(db=db,label_id=stat.label_id)
-#         # print(my_label)
-#         count = stat.count
-#         child_data = {
-#                     "child_name" : my_label[0].label_name,
-#                     "child_description" : my_label[0].label_description,
-#                     "count" : count,
-#                     "percent" : str((count/total_error)*100)
-#                 }
-#         if my_label[0].label_parent == "ROOT":
-#             for i in range(my_label[0].label_level-1):
-#                 my_label = dashboard_crud.get_label_by_label_name(db=db,label_name=my_label[0].label_parent)
-#             # parent = my_label.label_parent
-        
-#         parent = my_label[0].label_parent
-#         # if len(data) != 0:
-#         #     for i in data:
-#         #         print(data)
-#         #         if i['root_name'] != parent:
-#         #             data.append({ 
-#         #             "root_name" : parent,
-#         #             "data" : [child_data]})
-#         #         else : 
-#         #             print(type(i['data']))
-#         #             i['data'].append(child_data)
-#         # else : 
-#         #     data.append({ 
-#         #             "root_name" : parent,
-#         #             "data" : child_data})
-                
-#         data.append({ 
-#                 "root_name" : parent,
-#                 "child_data" : child_data})
-        
-#     roots = []
-#     child_name = []
-#     result = []
-#     for i in data:
-#         if (len(roots) == 0) or (i['root_name'] not in roots):
-#             result.append(i)
-#             roots.append(i['root_name'])
-#             child_name.append(i['child_data'][0]['child_name'])
-#         elif i['root_name'] in roots:
-#             if isinstance(i['child_data'],dict):
-#                 i['child_data'] =  
+    for stat in checked_document:
+        my_label = dashboard_crud.get_label_by_label_id(db=db,label_id=stat.label_id)
+        # print(my_label)
+        count = stat.count
+        child_data = {
+                    "child_name" : my_label[0].label_name,
+                    "child_description" : my_label[0].label_description,
+                    "count" : count,
+                    "percent" : str((count/total_error)*100)
+                }
+        if my_label[0].label_parent != "ROOT":
+            for i in range(my_label[0].label_level-1):
+                my_label = dashboard_crud.get_label_by_label_name(db=db,label_name=my_label[0].label_parent)
             
+            parent = my_label[0].label_parent    
+            data.append({ 
+                         "root_name" : parent,
+                         "data" : child_data})
             
-#     card_data = {
-#         "total" : str(total_document),
-#         "check" : str(len(checked_document)),
-#         "error" : str(total_error)
-#     }
+        else :
+            data.append({
+                "root_name":my_label[0].label_name,
+                "data" : child_data
+            })
+            
+    combined_data = {
+        "card_data": {
+            "total": str(total_document),
+            "check": str(len(checked_document)),
+            "error": str(total_error)},
+        "data": []
+        }
+
+    combined_by_root = {}
+    # print(data)
+    for item in data:
+        root_name = item["root_name"]
+        child_data = item.get("data")
     
-#     return {"card_data":card_data,
-#             "data":result
-#     }   
+        if root_name in combined_by_root:
+            # if combined_by_root[root_name]
+            count = 0
+            i = combined_by_root[root_name]
+            for j in range(len(i)):
+                if i[j]['child_name'] == child_data['child_name']:   
+                    combined_by_root[root_name][j]['count'] += 1
+                    count +=1
+                    
+            if count != 1:
+                combined_by_root[root_name].append(child_data)    
+            print(combined_by_root)
+        
+        else:
+            combined_by_root[root_name] = [child_data]
+            print(combined_by_root)
+
+    for root_name, data_list in combined_by_root.items():
+        combined_data["data"].append({
+            "root_name": root_name,
+            "data": data_list})
+    
+    return combined_data
         
     
     
