66892584021afd58e28f09931e059e77441016fd
phattradaa <phattrada.mik@dome.tu.ac.th>
2024-05-02 23:22:30 +0700

    commit

--- app/src/file_system/routers.py
+++ app/src/file_system/routers.py
@@ -7,9 +7,9 @@ from .exceptions import (validate_folder_name, check_existing)
 from starlette.background import BackgroundTasks
 from typing import List
 import os
-import time
-
+from src.dashboard.routers import create_stat
 from src.concordancer.crud import get_string
+
 router = APIRouter(
     tags=["File system"],
     prefix="/sys"
@@ -130,18 +130,39 @@ async def sys_delete_folder(folder_name:str):
         raise HTTPException(status_code=400, detail=f"{folder_name} is not existing")    
     
 @router.put("/save-file")
-async def sys_save_status_file(file:UploadFile, tagset_id:int):
+async def sys_save_status_file(background_tasks: BackgroundTasks, file:UploadFile, tagset_id:int, db:db_dependency):
+    print('eiei')
+    if file.content_type == "text/plain":    
+        file_name = file.filename
+        file_path = os.path.join("file", file_name)
+        data = ""
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+        
+        with open(file_path,"r") as f:
+            data += f.read()
     
-    #// Send text to endpoint API (string)
-    #// Save file
-    result, is_successful = global_st.save_state_file(file=file)
+        #// record stats
+        # create_stat(string=data, tagset_id=tagset_id, filename=file_name, db=db)
     
-    if is_successful:
-        return {"detail": f"Successfully update file {file.filename}"}
-    elif result == "File is not existing":
-        raise HTTPException(status_code=400, detail= f"{file.filename} is not existing in corpus")
+        #// Save file
+        result, is_successful = global_st.save_state_file(file_name=file_name)
+        
+        if is_successful:
+            background_tasks.add_task(clean_up,file_name)
+            resp = create_stat(string=data, tagset_id=tagset_id, filename=file_name, db=db)
+            if resp.content == "update dashboard successfully":
+                return {"detail": f"Successfully update file {file.filename}"}
+            else : 
+                return resp
+        elif result == "File is not existing":
+            background_tasks.add_task(clean_up,file_name)
+            raise HTTPException(status_code=400, detail= f"{file.filename} is not existing in corpus")
+        else:
+            background_tasks.add_task(clean_up,file_name)
+            raise HTTPException(status_code=500, detail="Service Unavailable, Google storage has a problem")    
     else:
-        raise HTTPException(status_code=500, detail="Service Unavailable, Google storage has a problem")    
+        raise HTTPException(status_code=400, detail="Wrong file extension")  
     
 @router.put("/change-blob-name", status_code=200)
 async def sys_rename_file(old_name:str, new_name:str):
